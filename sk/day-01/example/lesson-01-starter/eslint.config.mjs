4e65723d2e727adbc85bad7587df5ad70c4e9ddf
Dan Gilleland <dgilleland@nait.ca>
2026-01-16 20:14:59 -0700

    Update starter kit linting + package name

--- sk/day-01/example/lesson-01-starter/eslint.config.mjs
+++ sk/day-01/example/lesson-01-starter/eslint.config.mjs
@@ -1,12 +1,9 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import pluginCypress from 'eslint-plugin-cypress/flat';
 import stylistic from '@stylistic/eslint-plugin';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  pluginCypress.configs.globals,
-  pluginCypress.configs.recommended,
   pluginJs.configs.recommended,
   stylistic.configs.recommended,
   { languageOptions: { globals: globals.browser },
