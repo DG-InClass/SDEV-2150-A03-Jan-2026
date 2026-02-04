def0fb01325f8bb8be29d34d6e968d474502320b
Dan Gilleland <dgilleland@nait.ca>
2026-02-04 16:51:18 -0700

    JS Review - Primitives vs. Objects

--- src/js-quickstart/obj-ref.js
+++ src/js-quickstart/obj-ref.js
@@ -1,10 +1,13 @@
+// In the same folder as this file, run the following in the terminal:
+//      node --watch obj-ref.js
+
 let person = {
     name: 'Stewart Dent',
     age: 22
 };
 
 // References (points to) the SAME object as person
-let enrolled = person;
+let enrolled = person; // NOT a copy of everything inside person
 const phil = 'Phil Errup';
 console.log('Person Object:', person, '\nEnrolled Object:', enrolled);
 console.log(`\nChanging Enrolled object's name to: ${phil}\n`);
@@ -16,9 +19,9 @@ console.log();
 // Primitive types are "copied"
 // when assigning to new variables
 let teacher = "Dan Gilleland";
-let tutor = teacher;
+let tutor = teacher; // An actual copy of the string
 console.log(teacher, tutor);
-tutor = "Anna Lyst";
+tutor = "Anna Lyst"; // Changing tutor does NOT affect teacher
 console.log(teacher, tutor);
 
 // Time to draw some pictures on https://excalidraw.com/
