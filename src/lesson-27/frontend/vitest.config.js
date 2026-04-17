8aae826c02470f29750337e363167dda36e7c11f
Dan Gilleland <dgilleland@nait.ca>
2026-04-17 13:25:46 -0600

    Install testing dependencies + add configuration

---
+++ src/lesson-27/frontend/vitest.config.js
@@ -0,0 +1,16 @@
+// ~/vitest.config.js
+import { defineConfig } from 'vitest/config';
+import { playwright } from '@vitest/browser-playwright';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright(),
+            instances: [{ browser: 'chromium' }],
+        }
+    }
+});
